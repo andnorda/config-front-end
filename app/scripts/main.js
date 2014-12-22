1caae7f1868a28bd37ca67619bb9fa959eb0ac3b
Andreas Nordahl <andnorda@gmail.com>
2014-12-22 21:20:52 +0100

    moar structure

---
+++ app/scripts/main.js
@@ -0,0 +1,16 @@
+/*global ConfigFrontEnd, $*/
+window.ConfigFrontEnd = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    ConfigFrontEnd.init();
+});
